8bf317688eb59011e8bc9fd67b7dbaf108379dcb
Adam Vandenberg <flangy@gmail.com>
2013-08-06 21:04:48 -0700

    rabbitmq-c: use resource

--- Library/Formula/rabbitmq-c.rb
+++ Library/Formula/rabbitmq-c.rb
@@ -1,10 +1,5 @@
 require 'formula'
 
-class RabbitmqCodegen < Formula
-  url 'https://github.com/rabbitmq/rabbitmq-codegen/archive/rabbitmq_v3_1_0.tar.gz'
-  sha1 '5b82a3887586804a712aa2c5eeff4a59e89537b7'
-end
-
 class RabbitmqC < Formula
   homepage 'https://github.com/alanxz/rabbitmq-c'
   url 'https://github.com/alanxz/rabbitmq-c/archive/rabbitmq-c-v0.3.0.zip'
@@ -20,14 +15,16 @@ class RabbitmqC < Formula
   depends_on 'rabbitmq'
   depends_on 'simplejson' => :python if MacOS.version <= :leopard
 
+  resource 'codegen' do
+    url 'https://github.com/rabbitmq/rabbitmq-codegen/archive/rabbitmq_v3_1_0.tar.gz'
+    sha1 '5b82a3887586804a712aa2c5eeff4a59e89537b7'
+  end
+
   def install
     ENV.universal_binary if build.universal?
-
-    RabbitmqCodegen.new.brew { (buildpath/"codegen").install Dir["*"] }
-
+    (buildpath/'codegen').install resource('codegen')
     system "autoreconf", "-i"
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
     system "make install"
   end
-
 end
